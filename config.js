2312eaa1fbc45dae901ba4f10c770ca709973c8d
JoachimBye <163203695+JoachimBye@users.noreply.github.com>
2026-05-11 11:56:21 +0200

    Update Supabase URL to include full address

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // 3) Kopier "Project URL" og "anon public" key til feltene under
 // ============================================================
 
-const SUPABASE_URL      = 'hfqgrcwboeklcxccqslj';
+const SUPABASE_URL      = 'https://hfqgrcwboeklcxccqslj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmcWdyY3dib2VrbGN4Y2Nxc2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0ODEzNzMsImV4cCI6MjA5NDA1NzM3M30.-YpNN2xnnHtEOYGqXG8j4NIpX5ByFapeojcEW9s-Y9k';
 
 // Hvor mange sekunder pause mellom arbeidssett
